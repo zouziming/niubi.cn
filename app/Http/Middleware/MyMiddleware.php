ab217b9032608f6e85f6f589a24245102f658497
zfs <zfs@zhong.com>
2019-11-21 08:51:33 +0800

    0.0.8

--- app/Http/Middleware/MyMiddleware.php
+++ app/Http/Middleware/MyMiddleware.php
@@ -25,7 +25,7 @@ class MyMiddleware
         // dump($action_str);
         // 把控制器跟方法分割
         $action_arr=explode('@',$action_str);
-        dump($action_arr);
+        // dump($action_arr);
 
         // 获取控制器
         // $controller = $action_arr[0];
@@ -37,13 +37,13 @@ class MyMiddleware
         $power_list=[];
 
         // 通过用户查权限
-        /* $user_power=DB::table('user_has_permissions')
+        /* $user_power=DB::table('shop_user_has_permissions')
         ->where('uid',$use_id)
         ->get();
 
         foreach($user_power as $power) {
             查询出对应的权限详情
-            $power_tmp=DB::table('permissions')
+            $power_tmp=DB::table('shop_permissions')
             ->where('id',$power->permission_id)
             ->first();
             $name_tmp=$power_tmp->controller.'@'.$power_tmp->action;
@@ -53,6 +53,33 @@ class MyMiddleware
         }
         */
 
+
+        /*
+        //  通过角色查询权限
+        //  通过id查用户拥有的角色
+        $user_roles=DB::table('shop_user_has_roles')
+            ->where('uid',$use_id)
+            ->get();
+            dump($user_roles);
+            foreach($user_role as $role) {
+                //通过角色查角色的权限
+            $role_power=DB::table('shop_role_has_permissions')
+                ->where('role_id',$role->role_id)
+                ->get();
+
+            // 通过获取到权限id去获取权限详情
+            foreach($role_power as $power)
+            {
+            $power_tmp = DB::table('shop_permissions')
+                ->where('id',$power->permission_id)
+                ->first();
+
+                $name_tmp = $power_tmp->controller .'@'.$power_tmp->action;
+                $power_list[$name_tmp]=$name_tmp;
+            }
+        }
+        */
+        
         return $next($request);
     }
 }
