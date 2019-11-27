49104e6bb793c4b4b2f1c2b2b42e91a45b26cc63
zhu <1273332317@qq.com>
2019-11-27 20:13:32 +0800

    修改bug - 1.1.1

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::group(['prefix' => '/admin', 'middleware' => ['user.login']], function(){
 
     // 显示修改页面
     Route::get('/user/edit', 'Admin\UserController@edit');
-    // Route::post('/admin/user/edit', 'Admin\UserController@doedit');
+
     // 修改用户
     Route::post('/user/doedit', 'Admin\UserController@doedit');
     // 修改用户状态
@@ -63,69 +63,7 @@ Route::group(['prefix' => '/admin', 'middleware' => ['user.login']], function(){
     // 更换状态
     Route::get('/user/status/{id}/{status}', 'Admin\UserController@status');
 
-});
-    // 后台登录
-Route::get('/admin/login', 'Admin\LoginController@show');
-Route::post('/admin/login', 'Admin\LoginController@login');
-   
-
-// 搜索功能
-Route::get('/search', 'Admin\UserController@search');
-
-
-
-
-
-/**
- * 前台路由
- */
-// 前台首页
-Route::group(['prefix' => '/home', 'middleware' => ['users.login']], function(){
-    Route::get('/', 'Home\IndexController@index');
-
-    // 前台退出
-    Route::get('/logout', 'Home\IndexController@logout');
-});
-// 前台登录
-Route::get('/home/login', 'Home\LoginController@show');
-Route::post('/home/login', 'Home\LoginController@login');
-
-// 手机号登录
-Route::get('/home/logincode', 'Home\LoginController@showphone');
-Route::post('/home/logincode', 'Home\LoginController@logincode');
-Route::post('/home/dologincode', 'Home\LoginController@dologincode');
-
-
-/**
- *  前台注册
- */
-// 手机号注册
-Route::get('/home/register', 'Home\RegisterController@show');
-Route::post('/home/register', 'Home\RegisterController@register');
-Route::post('/home/doregister', 'Home\RegisterController@doregister');
-
-// 密码
-Route::get('/home/enroll', 'Home\RegisterController@reveal');
-Route::post('/home/enroll', 'Home\RegisterController@enroll');
-
-
-
-
-
-// 个人中心
-Route::get('/home/user/secure', 'Home\UserController@secure');
-Route::get('/home/user/mycenter', 'Home\UserController@mycenter');
-
-// 个人资料修改
-Route::post('/home/user/mycenter', 'Home\UserController@edit');
-// 修改头像
-Route::get('/home/user/picture', 'Home\UserController@pic');
-Route::post('/home/user/picture', 'Home\UserController@picture');
-// 修改密码
-Route::get('/home/user/password', 'Home\UserController@show');
-Route::post('/home/user/password', 'Home\UserController@password');
-
-Route::group(['prefix' => '/admin'], function(){
+// Route::group(['prefix' => '/admin'], function(){
 	//商品
     Route::get('/goods', 'Admin\GoodsController@index');
 	Route::get('/goods/search', 'Admin\GoodsController@search');
@@ -216,13 +154,10 @@ Route::group(['prefix' => '/admin'], function(){
 	Route::post('/lunbo/edit', 'Admin\LunboController@checkedit');
 	
 	Route::get('lunbo/del/{id}', 'Admin\LunboController@del');
-});
-
-
+// });
 // 分类
-Route::group(['prefix'=>'/admin','middleware' => ['power']],function() {
-	// 首页
-	Route::get('/','Admin\IndexController@index');
+// Route::group(['prefix'=>'/admin','middleware' => ['power']],function() {
+
 	// 分类列表
 	Route::get('/cate/index','Admin\CateController@index');
 	// 搜索
@@ -276,8 +211,71 @@ Route::group(['prefix'=>'/admin','middleware' => ['power']],function() {
 	// 修改管理员页面
 	Route::get('/power/userRole/edit/index','Admin\PowerController@userRoleEdit');
 	// 修改管理员
-	Route::post('/power/userRole/edit/index','Admin\PowerController@editUserRole');
+	// Route::post('/power/userRole/edit/index','Admin\PowerController@editUserRole');
+// });
+
+
+
 });
+
+    // 后台登录
+Route::get('/admin/login', 'Admin\LoginController@show');
+Route::post('/admin/login', 'Admin\LoginController@login');
+
+// 后台搜索功能
+Route::get('/search', 'Admin\UserController@search');
+
+
+
+
+
+/**
+ * 前台路由
+ */
+// 前台首页
+Route::group(['prefix' => '/home', 'middleware' => ['users.login']], function(){
+    Route::get('/', 'Home\IndexController@index');
+
+    // 前台退出
+    Route::get('/logout', 'Home\IndexController@logout');
+});
+// 前台登录
+Route::get('/home/login', 'Home\LoginController@show');
+Route::post('/home/login', 'Home\LoginController@login');
+
+// 手机号登录
+Route::get('/home/logincode', 'Home\LoginController@showphone');
+Route::post('/home/logincode', 'Home\LoginController@logincode');
+Route::post('/home/dologincode', 'Home\LoginController@dologincode');
+
+
+/**
+ *  前台注册
+ */
+// 手机号注册
+Route::get('/home/register', 'Home\RegisterController@show');
+Route::post('/home/register', 'Home\RegisterController@register');
+Route::post('/home/doregister', 'Home\RegisterController@doregister');
+
+// 密码
+Route::get('/home/enroll', 'Home\RegisterController@reveal');
+Route::post('/home/enroll', 'Home\RegisterController@enroll');
+
+// 个人中心
+Route::get('/home/user/secure', 'Home\UserController@secure');
+Route::get('/home/user/mycenter', 'Home\UserController@mycenter');
+
+// 个人资料修改
+Route::post('/home/user/mycenter', 'Home\UserController@edit');
+// 修改头像
+Route::get('/home/user/picture', 'Home\UserController@pic');
+Route::post('/home/user/picture', 'Home\UserController@picture');
+// 修改密码
+Route::get('/home/user/password', 'Home\UserController@show');
+Route::post('/home/user/password', 'Home\UserController@password');
+
+
+
 
 
 
