5c04cd56c6893fa9b115448382efa878ee32d3dd
zfs <zfs@zhong.com>
2019-12-02 09:05:22 +0800

    0.0.26

--- resources/views/Admin/power/userRole.blade.php
+++ resources/views/Admin/power/userRole.blade.php
@@ -24,7 +24,7 @@
                 <tr  style="font-size:10px">
                   <td>{{$v->username}}</td>
                   <td>{{$v->name}}</td>
-                  <td><a href="/admin/power/userRole/del?id={{$v->aid}}&uid={{$v->id}}">删除</a>|<a href="/admin/power/userRole/edit/index?id={{$v->aid}}&role_id={{$v->role_id}}&uid={{$v->username}}">编辑</a></td>
+                  <td><a class="del" data-uid="{{$v->id}}" data-id="{{$v->aid}}" href="javascript:void(0)">删除</a>|<a href="/admin/power/userRole/edit/index?id={{$v->aid}}&role_id={{$v->role_id}}&uid={{$v->username}}">编辑</a></td>
                 </tr>
                 @endforeach
               
@@ -33,4 +33,34 @@
         </div>
          <button type="button" class="btn btn-default"><a href="/admin/power/userRole/addIndex">添加管理员</a></button>
     </div>
+@endsection
+
+@section('script')
+  <script type="text/javascript">
+      $('.del').click(function(){
+        var _t=this;
+        var uid=$(this).data('uid');
+        var id=$(this).data('id');
+        // console.dir(uid);
+        // console.dir(id);
+        $.ajax({
+          type:'get',
+          url:'/admin/power/userRole/del',
+          data:{
+            uid:uid,
+            id:id,
+          },
+          success:function(res) {
+            if (res.code==0) {
+              // $(_t)
+              $(_t).parent().parent().remove()
+              // alert(res.msg);
+            } else {
+              alert(res.msg);
+            }
+          }
+        })
+        return false;
+      })
+  </script>
 @endsection
